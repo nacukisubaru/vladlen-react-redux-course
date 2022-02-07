dadca3bd8333a6a1c065a8f0e60cba1bd16ec2a3
nacukisubaru <alexitsheff@gmail.com>
2022-02-07 14:42:31 +0300

    каркас приложения

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="container pt-3">
+            <div class="row">
+				<div className="col">
+					FORM
+				</div>
+			</div>
+			<div class="row">
+				<div className="col">
+					POSTS
+				</div>
+				<div className="col">
+					FETCHED
+				</div>
+			</div>
+        </div>
+    );
 }
 
 export default App;
