c11852efdc46c7782a761612df842f26ca32b6b7
nacukisubaru <alexitsheff@gmail.com>
2022-02-07 19:43:26 +0300

    установка и подключение middleware

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {createStore} from 'redux'
+import {compose, createStore, applyMiddleware} from 'redux'
+import thunk from 'redux-thunk';
 import {Provider} from 'react-redux'
 import { rootReducer } from "./redux/index.js";
-import { compose } from 'redux';
 
 const store = createStore(rootReducer, compose(
+  applyMiddleware(thunk),
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 ))
 
